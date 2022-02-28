659b44e5e9ba18bcfff7bbc5c84258a8894cd88a
Nguyen Trong Duc Huy <76842097+duchuy1612@users.noreply.github.com>
2022-02-28 17:08:02 +0700

    Update Question1.c

--- Chapter 5 - Process APIs/Question1.c
+++ Chapter 5 - Process APIs/Question1.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
-#include <stdlib.h> // exit
+#include <stdlib.h>
 #include <sys/wait.h>
-#include <unistd.h> // fork
+#include <unistd.h>
 
-int main() {
-  int x = 100;
-  int rc = fork();
-  if (rc < 0) {
-    // fork failed; exit
-    fprintf(stderr, "fork failed\n");
-    exit(EXIT_FAILURE);
-  } else if (rc == 0) {
-    x = 101;
-    printf("x in child process: %d\n", x);
-  } else {
-    x = 102;
-    printf("x in parent process: %d\n", x);
-    wait(NULL);
-  }
+int main(){
+    int x = 100;
+    int rc = fork();
+    if (rc < 0){
+      fprintf(stderr, "Fork failed\n");
+      exit(EXIT_FAILURE);
+    } 
+    else if (rc == 0){
+      x = 101;
+      printf("x is in child process: %d\n", x);
+    } 
+    else{
+      x = 102;
+      printf("x is in parent process: %d\n", x);
+      wait(NULL);
+    }
   return 0;
 }
